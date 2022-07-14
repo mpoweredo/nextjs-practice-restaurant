ad55182d6164bc1ca9055e787f327461d53587d4
mpoweredo <mpow3r1337@gmail.com>
2022-07-14 13:12:09 +0200

    adding react import in error Boundary 3nd time

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import ErrorBoundary from '../components/errourBoundary/ErrorBoundary';
 import MainNavigation from '../components/layout/MainNavigation';
 import '../styles/globals.css';
 import React from 'react';
@@ -11,9 +10,7 @@ function MyApp({ Component, pageProps }) {
 				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
 			</Head>
 			<MainNavigation />
-			<ErrorBoundary>
-				<Component {...pageProps} />
-			</ErrorBoundary>
+			<Component {...pageProps} />
 		</>
 	);
 }
